4516e7dddabba5ed8bb3e0a43a189e59f63fcd54
paulchin <paulchin20@yahoo.com>
2018-11-26 00:47:15 +0800

    first

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignIn from './src/SignIn';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <SignIn />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+});
---
+++ src/SignIn.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Text, View, ImageBackground } from 'react-native';
+const myBackground = require('../assets/landing.jpg');
+import { Form, Item, Label, Input, Button } from 'native-base';
+
+export class SignIn extends Component {
+  state = {
+    email: '',
+    password: ''
+  };
+
+  login = () => {};
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <ImageBackground source={myBackground} style={styles.imgBg}>
+          <View style={styles.tmp}>
+            <Form>
+              <Item floatingLabel>
+                <Label>Email</Label>
+                <Input
+                  style={styles.tmp}
+                  autoCorrect={false}
+                  onChangeText={email => {
+                    this.setState({ email: email });
+                  }}
+                />
+              </Item>
+              <Item floatingLabel>
+                <Label>Password</Label>
+                <Input
+                  style={styles.tmp}
+                  autoCorrect={false}
+                  onChangeText={password => {
+                    this.setState({ password: password });
+                  }}
+                  secureTextEntry
+                />
+              </Item>
+            </Form>
+            <View style={{ marginTop: 10 }}>
+              <Button primary block onPress={this.login}>
+                <Text>Sign In/Sign Up</Text>
+              </Button>
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+const styles = {
+  tmp: {
+    flex: 1
+  },
+  imgBg: {
+    width: '100%',
+    height: '100%'
+  }
+};
+
+export default SignIn;
